3d9de8aed0d1fc5dfecdba277b7353e721f88df0
Holly Prothe <hollyprothe1@gmail.com>
2020-04-20 18:20:17 -0500

    add variable for city

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,12 +1,13 @@
 var queryUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCehscpkC6zkVEWlEvPL-Ja64zJxeXMGmU&callback=initMap"
+var city = ".expand"
 
 function initMap() {
     var directionsService = new google.maps.DirectionsService();
     var directionsRenderer = new google.maps.DirectionsRenderer();
-    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+    var city = new google.maps.LatLng("city");
     var mapOptions = {
         zoom: 7,
-        center: chicago
+        center: city
     }
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
     directionsRenderer.setMap(map);
